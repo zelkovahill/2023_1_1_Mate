b1dd4221826040d7fd72fa01307b75105c1a2caa
zelkovahill <changwoomin426@gmail.com>
2023-06-05 11:29:15 +0900

    씬 버튼

---
+++ Mate_/Assets/02_Code/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+    public void Button()
+    {
+        Debug.Log("∫¥»∆«œ¿Ã");
+    }
+}
