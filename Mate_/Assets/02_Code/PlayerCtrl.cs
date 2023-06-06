a32182fca35f5699ae18f7451abbf3bfbe8983a7
Leeyunseok77 <qhdksrjp22@naver.com>
2023-06-06 18:15:21 +0900

    건물 배치

---
+++ Mate_/Assets/02_Code/PlayerCtrl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    // 컴포넌트를 처리할 변수
+    [SerializeField]
+    private Transform tr;
+    // 이동 속력 변수 (public으로 선언되어 인스펙터 뷰에 노출됨)
+    public float moveSpeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Transform 컴포넌트를 추출해 변수에 대입
+        tr = GetComponent<Transform>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ +1.0f
+        float v = Input.GetAxis("Vertical"); // -1.0f ~ 0.0f ~ +1.0f
+        Debug.Log("h=" + h);
+        Debug.Log("v=" + v);
+        // 전후좌우 이동 방향 벡터 계산
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        // Translate(이동 방향 * 속력 * Time.deltaTime)
+        tr.Translate(moveDir * Time.deltaTime * moveSpeed);
+    }
+}
