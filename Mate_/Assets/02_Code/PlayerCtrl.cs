125f85d5caa7e62f46314c510070e8fcb725fd3b
Leeyunseok77 <qhdksrjp22@naver.com>
2023-06-12 12:40:20 +0900

    6월12일 작업

--- Mate_/Assets/02_Code/PlayerCtrl.cs
+++ Mate_/Assets/02_Code/PlayerCtrl.cs
@@ -9,18 +9,51 @@ public class PlayerCtrl : MonoBehaviour
     public float jumpForce = 5.0f;
     private bool isJumping = false;
 
+    public Animator anim;
+    private readonly int hashWalk = Animator.StringToHash("isWalking");
+    private readonly int hashJumo = Animator.StringToHash("isJump");
+
+    public Transform tr_Player;
+ 
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
+        //anim = GetComponent<Animator>();
+    }   
 
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+       
 
-        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        Vector3 moveDir =  new Vector3(1.0f, 0.0f , 0.0f ) * h;
         transform.Translate(moveDir * Time.deltaTime * moveSpeed);
+
+        if (h != 0)
+        {
+            anim.SetBool("isWalking", true);
+            Debug.Log(hashWalk.ToString());
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isJump", false);
+        }
+
+        if(h >= 0)
+        {           
+            Vector3 currentEulerAngles = new Vector3(0.0f, 90.0f, 0.0f);
+           
+            tr_Player.transform.eulerAngles = currentEulerAngles;
+        }
+        else
+        {
+            Vector3 currentEulerAngles = new Vector3(0.0f, 270.0f, 0.0f);
+
+            tr_Player.transform.eulerAngles = currentEulerAngles;
+        }
+
 
         if (Input.GetButtonDown("Jump") && !isJumping)
         {
@@ -32,6 +65,7 @@ public class PlayerCtrl : MonoBehaviour
     {
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         isJumping = true;
+        anim.SetTrigger(hashJumo);
     }
 
     void OnCollisionEnter(Collision collision)
